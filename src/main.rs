c939b5aef2bbbf81e6a66b1fc3d4a2c084701ab9
Mario Dambros Filho <dambros@gmail.com>
2020-12-28 20:11:26 -0300

    Add scanlines

--- src/main.rs
+++ src/main.rs
@@ -105,10 +105,12 @@ impl GameState for State {
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
+    let mut context = RltkBuilder::simple80x50()
         .with_title("Roguelike In Rust")
         .with_fps_cap(30.0)
         .build()?;
+    context.with_post_scanlines(true);
+    
     let mut gs = State {
         ecs: World::new(),
     };
